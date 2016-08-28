4d35017f9769844a24c652c88b8d3acc25b188bf
Petr Posvic <petr@posvic.cz>
2016-08-28 20:01:56 +0200

    Optimize python script

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-# dmenu_priority
+# dmenu priority filter
+
 Priority filter for dmenu. Shows often used commands on top.
 
-Modify /usr/local/dmenu_run file to apply this filter. Examples in dmenu_run.sh.
+Modify /usr/local/bin/dmenu_run (or /usr/bin/dmenu_run) file to apply this filter.
+
+Use filter with **merge** parameter before any execution. It merges generated file from dmenu and this filter and
+creates new dmenu file where frequently used application are on top. 
+```
+main.py merge
+```
+
+Use filter with **update** parameter after dmenu processing. It increases number of launches for launching application
+and updates file for this filter. 
+```
+main.py update
+```
+
+## Example
+```
+#!/bin/sh
+cachedir=${XDG_CACHE_HOME:-"$HOME/.cache"}
+if [ -d "$cachedir" ]; then
+        cache=$cachedir/dmenu_run
+else
+        cache=$HOME/.dmenu_cache # if no xdg dir, fall back to dotfile in ~
+fi
+
+/home/petr/Programy/dmenu_priority/main.py merge
+
+(
+        IFS=:
+        if stest -dqr -n "$cache" $PATH; then
+                stest -flx $PATH | sort -u | tee "$cache" | dmenu "$@"
+        else
+                dmenu "$@" < "$cache"
+        fi
+) | /home/petr/Programy/dmenu_priority/main.py update | ${SHELL:-"/bin/sh"} &
+```
---
+++ main.py
@@ -0,0 +1,101 @@
+#!/usr/bin/python2.7
+import sys
+import os
+
+FILE_COUNT = "/home/petr/.cache/dmenu_count"
+FILE_RUN = "/home/petr/.cache/dmenu_run"
+
+
+def clean():
+    if os.path.isfile(FILE_COUNT):
+        print("Remove file {0}".format(FILE_COUNT))
+        os.remove(FILE_COUNT)
+    if os.path.isfile(FILE_RUN):
+        print("Remove file {0}".format(FILE_RUN))
+        os.remove(FILE_RUN)
+
+
+def merge():
+    commands = []
+
+    if not os.path.isfile(FILE_RUN):
+        return
+
+    # Read all used commands
+    with open(FILE_COUNT, "r") as f:
+        for cmd in f.readlines():
+            i = cmd.find(" ") + 1
+            if i > 0:
+                commands.append(cmd[:i].strip() + '\n')
+
+    # Read all system commands
+    with open(FILE_RUN, "r") as f:
+        for command in f.readlines():
+            if command not in commands:
+                commands.append(command)
+
+    with open(FILE_RUN, "w") as f:
+        f.writelines(commands)
+
+
+def update(command):
+    commands = []
+
+    # Move up or add command
+    with open(FILE_COUNT, "r") as f:
+
+        # Add new command at bottom if i doesn't exist in command list
+        new = True
+
+        for cmd in f.readlines():
+            i = cmd.find(" ") + 1
+            if i == 1:
+                continue
+
+            key = cmd[:i].strip()
+            val = cmd[i:].strip()
+
+            if key == command:
+                val = str(int(val) + 1)
+                new = False
+
+            commands.append((
+                key, val
+            ))
+
+        if new:
+            commands.append((
+                command, "1"
+            ))
+
+    commands.sort(key=lambda x: x[1], reverse=True)
+    with open(FILE_COUNT, "w") as f:
+        for cmd in commands:
+            f.write(str(cmd[0]) + ' ' + str(cmd[1]) + '\n')
+
+    print(command + '\n')
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        exit(1)
+
+    op = sys.argv[1]
+    if op == "clean":
+        clean()
+        exit(0)
+
+    # Create file if it doesn't exist
+    if not os.path.isfile(FILE_COUNT):
+        with open(FILE_COUNT, "w"):
+            pass
+
+    # Note: FILE_RUN should be created by dmenu
+
+    if op == "merge":
+        merge()
+    elif op == "update":
+        readlines = sys.stdin.readlines()
+        if len(readlines) > 0:
+            cmd = readlines[0].strip()
+            update(cmd)
